fc54a64114d5c35cb751c459ca9325aeb6329a52
mialco <mikeo@mialco.com>
2024-03-01 00:17:18 -0500

    change the behavior of selecting the categories based on groups

--- src/app/core/category-groups/category-groups.component.ts
+++ src/app/core/category-groups/category-groups.component.ts
@@ -24,7 +24,7 @@ export class CategoryGroupsComponent
 
         //And I want to esure that the groups are all selected by default
         this.selectedOptions.forEach(group => {
-          this.groupSelected(group);
+          this.groupSelected(group, true);
           //group.selected = true;
           //this.productsService.selectedGroups.set(group.id, group.selected);
           // The UI does not reflect the changes in the selected property of the group
@@ -33,12 +33,35 @@ export class CategoryGroupsComponent
     );
   }
 
-  groupSelected(group: CategoryGroup) {
+  allSelectedOptions : number[] = [];//  = this.selectedOptions.map(group => group.id);
+  // trackByFn(item: CategoryGroup): number {
+  //   return item.id;
+  // }
+  // compareFn(groupA: CategoryGroup, groupB: CategoryGroup): boolean {
+  //   return groupA.id === groupB.id;
+  // }
+
+  groupSelected(group: CategoryGroup, initialize:boolean) {
+    console.log('groupSelected  - Initialize: ' + initialize);
     if (group.selected == undefined) {
       group.selected = false;
     }
-    group.selected = !group.selected;
-    this.productsService.selectedGroups.set(group.id, group.selected);
+
+    if (initialize) {
+      this.allSelectedOptions = this.selectedOptions.map(group => group.id);
+      group.selected = true;
+      this.productsService.selectedGroups.set(group.id, group.selected);
+      const foundGroup = this.selectedOptions.find(g => g.id == group.id);
+      if (foundGroup) {
+        foundGroup.selected = group.selected;
+      }
+      return;
+    } else {
+      //group.selected = !group.selected;
+    }
+    console.log('groupSelected  - group.selected: ' + group);
+    let selectedGroupId = this.allSelectedOptions.find(g => g == group.id) ;
+    this.productsService.selectedGroups.set(group.id, selectedGroupId === undefined );
     this.productsService.updateCategoryTreeOnGroupChange();
   }
 
